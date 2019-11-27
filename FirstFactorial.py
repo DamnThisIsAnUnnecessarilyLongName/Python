d7bbc78891946fa60755041f47201fa0a4ddb400
u6411134 <u6411134@anu.edu.au>
2019-11-27 21:57:04 +1100

    saving stuff

---
+++ FirstFactorial.py
@@ -0,0 +1,10 @@
+from pip._vendor.distlib.compat import raw_input
+
+
+def FirstFactorial(num):
+    # code goes here
+    return num
+
+# keep this function call here
+print
+FirstFactorial(raw_input());
---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+# print('Hello World');
+# name = 'Marcus';
+# print('Welcome ' + name);
+import numpy as np
+import pandas as pd
+import csv
+
+# x = 14;
+# if x == 14:
+#     print('x is equal to ' + str (x));
+
+s = pd.Series ([1,3,5,5,6,7]);
+print(s);
